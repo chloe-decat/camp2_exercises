6a00ddace2b8bb772684d46525d300b0136696f2
chloe bataille <chloe.bataille@decathlon.com>
2017-11-24 13:28:49 +0100

    modif des exos de vendredi

--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/05_descending.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/05_descending.js
@@ -3,3 +3,6 @@
 // -  Using `length`, write on `stdout` each values of the `litteralDigits` array, descending.
 
 const litteralDigits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+for (let i = 10 ; i > litteralDigits.length; i = i--){
+  console.log(litteralDigits[i]);
+}
