c367bfd288059cd9433f860ca0fedf935ac317bb
chloe bataille <chloe.bataille@decathlon.com>
2017-12-06 11:09:38 +0100

    sacré postman

---
+++ 02_programming_fundamentals/lesentrainementsdechloe.js
@@ -0,0 +1,3 @@
+//ajouter une variable avec mon bureau
+//ajouter les elements de mon bureau (ordi, cahier, lampe, chaise, pot a crayons, tiroir)
+// ajouter des elements dans mon tiroir (colle, scotch, trousse, bouteille d'eau)
