79618861e0b319ce92773499144cc1a6d6d30dc4
chloe bataille <chloe.bataille@decathlon.com>
2017-12-15 11:17:05 +0100

    projet de chloe api

--- 03_backend_developement/le-projet-de-chloe-api/api_call.js
+++ 03_backend_developement/le-projet-de-chloe-api/api_call.js
@@ -1,94 +1,175 @@
-const request = require("request");
-const { Pool } = require('pg')
-const pool = new Pool()
+const request = require('request');
+const { Pool } = require('pg');
+const pool = new Pool();
 
-function apicall(parameter,callback){
-request(
-  {
-    url: `https://decath-product-api.herokuapp.com/${parameter}`,
-    method: "GET",
-  },
-  function(error, response, result) {
-    if (error) {
-      console.warn("error:", error);
+function renderproductsbycategories(ProductsByCategoriesJSON,identifier){
+  const parsedResult = JSON.parse(ProductsByCategoriesJSON);
+  pool.connect(
+    (error, client, release) =>{
+      if (error){
+        release();
+        return console.error('Error acquiring client', error.stack);
+      }
+      else
+      {
+        parsedResult.map((newResult) => {
+          return client.query(
+            'INSERT INTO categories_products (categories_id,products_id) VALUES ($1,$2);',
+            [identifier,newResult.id],
+            (error, ProductsByCategoriesJSON)=>{
+              if (error){
+                return console.error('Error acquiring client', error.stack);
+              }
+              return console.log(ProductsByCategoriesJSON);
+            }
+          );
+        }
+        );
+        release();
+      }
     }
-    else {
-      callback(result);
+  );
+}
+
+function apicallproductsbycategories(identifier,renderproductsbycategoriescallback){
+  request(
+    {
+      url: `https://decath-product-api.herokuapp.com/categories/${identifier}/products`,
+      method: 'GET',
+    },
+    function(error, response, ProductsByCategoriesJSON) {
+      if (error) {
+        console.warn('error:', error);
+      }
+      else {
+        renderproductsbycategoriescallback(ProductsByCategoriesJSON,identifier);
+      }
     }
-    });
+  );
+}
+
+function renderIdentifier(resultJSON, apicallproductsbycategoriescallback){
+  setTimeout (() => {
+  for (let i=1; i<400;i++){
+    const identifier = JSON.parse(resultJSON)[i].id;
+    apicallproductsbycategoriescallback(identifier, renderproductsbycategories);
+  }} , 100000);
+  setTimeout (() => {
+  for (let j=400; j<800;j++){
+    const identifier = JSON.parse(resultJSON)[j].id;
+    apicallproductsbycategoriescallback(identifier, renderproductsbycategories);
+  }} , 100000);
+  for (let k=800; k<1002;k++){
+    const identifier = JSON.parse(resultJSON)[k].id;
+    apicallproductsbycategoriescallback(identifier, renderproductsbycategories);
+}
+}
+
+function apicallparameter (parameter, renderIdentifierCallback) {
+  request(
+    {
+      url: `https://decath-product-api.herokuapp.com/${parameter}`,
+      method: 'GET'
+    },
+    function(error, response, resultJSON) {
+      if (error){
+        console.warn('error:', error);
+      }
+      else {
+        renderIdentifierCallback(resultJSON, apicallproductsbycategories);
+      }
     }
+  );
+}
+
+apicallparameter('categories', renderIdentifier);
+
+
 
 function renderBrands(result){
-  const parsResult =JSON.parse(result);
-
-  pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack)
-    }
-
-    parsResult.map(function(newResult) {
-      return client.query(
-        "INSERT INTO brands VALUES ($1,$2);",
-        [newResult.id, newResult.title],
-        (err, result) => {
-          release()
-          if (err) {
-            return console.error('Error executing query', err.stack)
-          }
-          console.log(result.rows)
+  const parsedResult = JSON.parse(result);
+  pool.connect(
+    (error, client, release) =>{
+      if (error){
+        release();
+        return console.error('Error acquiring client', error.stack);
+      }
+      else
+      {
+        parsedResult.map((newResult) => {
+          return client.query(
+          'INSERT INTO brands VALUES ($1, $2);',
+            [newResult.id,newResult.title],
+            (error, result)=>{
+              if (error){
+                return console.error('Error acquiring client', error.stack);
+              }
+              return console.log(result);
+            }
+          );
         }
-      )
-    })
-  })
-};
-
+        );
+        release();
+      }
+    }
+  );
+}
 function renderProducts(result){
-  const parsResult =JSON.parse(result);
-
-  pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack)
-    }
-
-    parsResult.map(function(newResult) {
-      return client.query(
-        "INSERT INTO products VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);",
-        [newResult.id, newResult.decathlon_id,newResult.title, newResult.description,newResult.brand_id, newResult.min_price,newResult.max_price, newResult.crossed_price,newResult.percent_reduction,newResult.image_path, newResult.rating],
-        (err, result) => {
-          release()
-          if (err) {
-            return console.error('Error executing query', err.stack)
-          }
-          console.log(result.rows)
+  const parsedResult = JSON.parse(result);
+  pool.connect(
+    (error, client, release) =>{
+      if (error){
+        release();
+        return console.error('Error acquiring client', error.stack);
+      }
+      else
+      {
+        parsedResult.map((newResult) => {
+          return client.query(
+            'INSERT INTO Products VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11);',
+            [newResult.id,newResult.decathlon_id,newResult.title,newResult.description,newResult.brand_id,newResult.min_price,newResult.max_price,newResult.crossed_price,newResult.percent_reduction,newResult.image_path,newResult.rating],
+            (error, result)=>{
+              if (error){
+                return console.error('Error acquiring client', error.stack);
+              }
+              return console.log(result);
+            }
+          );
         }
-      )
-    })
-  })
-};
-
+        );
+        release();
+      }
+    }
+  );
+}
 function renderCategories(result){
-  const parsResult =JSON.parse(result);
-
-  pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack)
-    }
-
-    parsResult.map(function(newResult) {
-      return client.query(
-        "INSERT INTO categories VALUES ($1,$2,$3);",
-        [newResult.id, newResult.decathlon_id,newResult.label],
-        (err, result) => {
-          release()
-          if (err) {
-            return console.error('Error executing query', err.stack)
-          }
-          console.log(result.rows)
+  const parsedResult = JSON.parse(result);
+  pool.connect(
+    (error, client, release) =>{
+      if (error){
+        release();
+        return console.error('Error acquiring client', error.stack);
+      }
+      else
+      {
+        parsedResult.map((newResult) => {
+          return client.query(
+            'INSERT INTO categories VALUES ($1, $2, $3);',
+            [newResult.id,newResult.decathlon_id,newResult.label],
+            (error, result)=>{
+              if (error){
+                return console.error('Error acquiring client', error.stack);
+              }
+              return console.log(result);
+            }
+          );
         }
-      )
-    })
-  })
-};
-apicall("brands",renderBrands);
-apicall("products",renderProducts);
-apicall("categories",renderCategories);
+        );
+        release();
+      }
+    }
+  );
+}
+// apicallparameter(“brands”,renderBrands);
+// apicallparameter(“products”,renderProducts);
+// apicallparameter(“categories”,renderCategories);
--- 03_backend_developement/le-projet-de-chloe-api/server.js
+++ 03_backend_developement/le-projet-de-chloe-api/server.js
@@ -1,11 +1,80 @@
 const express = require("express");
 const app = express();
-
+const PG = require('pg')
 const port = process.env.PORT || 3000;
 
-app.get("/", function (request, result) {
-  result.json("Hello World!");
-});
+function route(parameter){
+  app.get(
+   `/${parameter}`,
+   function(request, result) {
+     const client = new PG.Client();
+     client.connect();
+     client.query(
+       `SELECT * FROM ${parameter};`,
+       function(error, resultQuery) {
+         if (error) {
+           result.send(error);
+         } else {
+           result.json(resultQuery.rows);
+         }
+         client.end();
+       }
+     );
+   }
+ );
+}
+
+route("categories");
+route("brands");
+route("products");
+
+
+function routeId(parameter){
+  app.get(
+   `/${parameter}/:id`,
+   function(request, result) {
+    // request database
+    const client = new PG.Client();
+    client.connect();
+    client.query(
+      `SELECT * FROM ${parameter} WHERE id = $1;`,
+      [request.params.id],
+      function(error, resultQuery) {
+        if (error) {
+          result.send(error);
+        } else {
+          result.json(resultQuery.rows);
+        }
+        client.end();
+      }
+    );
+   }
+  );
+}
+
+routeId("categories");
+routeId("brands");
+routeId("products");
+
+  app.get(
+   "/categories/:id/products",
+   function(request, result) {
+    const client = new PG.Client();
+    client.connect();
+    client.query(
+      `SELECT products.* FROM products INNER JOIN categories_products ON (products.id = categories_products.products_id) WHERE categories_products.categories_id = $1;`,
+      [request.params.id],
+      function(error, resultQuery) {
+        if (error) {
+          result.send(error);
+        } else {
+          result.json(resultQuery.rows);
+        }
+        client.end();
+      }
+    );
+   }
+  );
 
 app.listen(port, function () {
   console.log("Server listening on port:" + port);
